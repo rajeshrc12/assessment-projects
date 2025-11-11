a0a29c99a22fd8bb04132fc2f4459ad8ed6672ce
Rajesh Charhajari <rajesh.charhajari@gmail.com>
2025-11-11 07:13:18 +0000

    env variable added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express()
-
+const port = process.env.PORT
 app.get("/",(req,res)=>{
     res.json({message:"hello"})
 })
 
-app.listen(3001,()=>{
-    console.log("server is listening on 3001")
+app.listen(port,()=>{
+    console.log(`server is listening on ${port}`)
 })
