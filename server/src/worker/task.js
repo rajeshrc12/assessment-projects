6d676c3aaae1fe1fe180106ba9e342eb15f87dca
Rajesh Charhajari <rajesh.charhajari@gmail.com>
2025-11-19 14:00:32 +0000

    worker console

--- server/src/worker/task.js
+++ server/src/worker/task.js
@@ -1,11 +1,13 @@
 export default async (task) => {
-  const { id, time } = task;
-  console.log(`Task ${id} is executing...`);
+  const { id, time, userId, jobId } = task;
+  console.log(
+    `UserId: ${userId}, JobId:${jobId}, TaskId:${id} is executing...`
+  );
   await new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, time * 1000);
   });
-  console.log(`Task ${id} is executed`);
+  console.log(`UserId: ${userId}, JobId:${jobId} TaskId:${id} is executed.`);
   return task;
 };
